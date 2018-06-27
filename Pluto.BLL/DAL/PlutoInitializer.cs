01c627498eaf7c0dd9e9c0005bf840a35b385401
Marci6 <zmarci14@gmail.com>
2018-06-27 11:51:45 +0200

    DbContext and initialization, Subject class

---
+++ Pluto.BLL/DAL/PlutoInitializer.cs
@@ -0,0 +1,30 @@
+﻿using Pluto.BLL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pluto.BLL.DAL
+{
+    public class PlutoInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<PlutoContext>
+    {
+        protected override void Seed(PlutoContext context)
+        {
+            var subjects = new List<Subject>
+            {
+                new Subject{ Name = "Analízis", Credit = 6 },
+                new Subject{ Name = "Bevezetés  a számításelméletbe", Credit = 4 },
+                new Subject{ Name = "Fizika", Credit = 4 },
+                new Subject{ Name = "Digitális technika", Credit = 7 },
+                new Subject{ Name = "A programozás alapjai", Credit = 7 }
+            };
+
+            subjects.ForEach(s => context.Subjects.Add(s));
+
+            context.SaveChanges();
+
+            base.Seed(context);
+        }
+    }
+}
