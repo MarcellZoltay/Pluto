c1df93f3dd7c3afe3baabac190a320f6fbb64a57
Marci6 <zmarci14@gmail.com>
2018-06-27 13:39:57 +0200

    WPF navigation

---
+++ Pluto.Wpf/Views/StartPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Pluto.Wpf.Views
+{
+    /// <summary>
+    /// Interaction logic for StartPage
+    /// </summary>
+    public partial class StartPage : UserControl
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
